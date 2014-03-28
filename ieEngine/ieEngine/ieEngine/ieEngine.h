3d8c47c6416f2a83eb38c520c9a2fe16a1597a95
sergey.sergeev <codeoneclick@Mac-mini-Valeriy.local>
2014-03-28 11:27:22 +0200

    Initial Commit

---
+++ ieEngine/ieEngine/ieEngine/ieEngine.h
@@ -0,0 +1,13 @@
+//
+//  ieEngine.h
+//  ieEngine
+//
+//  Created by sergey.sergeev on 3/28/14.
+//  Copyright (c) 2014 sergey.sergeev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ieEngine : NSObject
+
+@end
---
+++ ieEngine/ieEngine/ieEngine/ieEngine.m
@@ -0,0 +1,13 @@
+//
+//  ieEngine.m
+//  ieEngine
+//
+//  Created by sergey.sergeev on 3/28/14.
+//  Copyright (c) 2014 sergey.sergeev. All rights reserved.
+//
+
+#import "ieEngine.h"
+
+@implementation ieEngine
+
+@end
